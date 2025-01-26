a5b8892a24a98432b3ec4a720b231ea869fef527
José Augusto C F Oliveira <gutinho.bahia.ja12@gmail.com>
2025-01-26 14:51:07 -0300

    feat: add process.env

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,26 @@
 import fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 import { userRoutes } from './routes/user.routes'
-import  {contactsRoutes} from './routes/contact.routes'
+import { contactsRoutes } from './routes/contact.routes'
 const app: FastifyInstance = fastify({ logger: true });
 
+const port = process.env.PORT || 3100;
 
 app.register(cors, {
   origin: "*", // Permite todas as origens (ou especifique origens permitidas)
   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
 });
-app.register(userRoutes,{
+app.register(userRoutes, {
   prefix: '/users',
- });
+});
 
-app.register(contactsRoutes,{
+app.register(contactsRoutes, {
   prefix: '/contacts',
- });
+});
 
 app.listen(
   {
-  port: 3100
+    port
   },
-() => console.log('Server is running on port 3100'),
+  () => console.log(`Server is running on port ${port}`),
 );
