6019790c977e1403165ebf598034734a88f305b3
José Augusto C F Oliveira <gutinho.bahia.ja12@gmail.com>
2025-01-26 11:36:55 -0300

    feat: tentativa de correção da rota de comunicação

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,22 @@
 import fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
-import { userRoutes } from './routes/user.routes'
-import  {contactsRoutes} from './routes/contact.routes'
+import { userRoutes } from './routes/user.routes';
+import { contactsRoutes } from './routes/contact.routes';
 
 const app: FastifyInstance = fastify({ logger: true });
-
-const port = parseInt(process.env.PORT || "3100", 10);
 
 app.register(cors, {
   origin: "*", // Permite todas as origens (ou especifique origens permitidas)
   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
 });
 
-app.register(userRoutes,{
+app.register(userRoutes, {
   prefix: '/users',
- });
+});
 
-app.register(contactsRoutes,{
+app.register(contactsRoutes, {
   prefix: '/contacts',
- });
+});
 
-app.listen(
-  { port },
-() => console.log(`Server is running on port ${port}`),
-);
+// Exportando o app Fastify diretamente, sem usar app.listen
+export default app;
